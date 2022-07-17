e2380eb90a9dc24a2dd2d59c62e8f5491ca284eb
Yann GESLIN <ygeslin@e1r6p15.clusters.42paris.fr>
2022-07-17 14:16:44 +0200

    replace age int field by interface

--- models/models.go
+++ models/models.go
@@ -4,22 +4,22 @@ type Friend struct{
     Name        string     `json:"name"`
 }
 type User struct {
-    Id          string     `json:"id"`
-    Password    string     `json:"password"`
-    IsActive    bool       `json:"isActive"`
-    Balance     string     `json:"balance"`
-    Age         int        `json:"age"`
-    Name        string     `json:"name"`
-    Gender      string     `json:"gender"`
-    Company     string     `json:"company"`
-    Email       string     `json:"email"`
-    Phone       string     `json:"phone"`
-    Address     string     `json:"address"`
-    About       string     `json:"about"`
-    Registered  string     `json:"registered"`
-    Latitude    float32    `json:"Latitude"`
-    Longitude   float32    `json:"Longitude"`
-    Tags        []string   `json:"tags"`
-    Friends     []Friend   `json:"friends"`
-    Data        string     `json:"data"`
+    Id          string        `json:"id"`
+    Password    string        `json:"password"`
+    IsActive    bool          `json:"isActive"`
+    Balance     string        `json:"balance"`
+    Age         interface{}   `json:"age"`
+    Name        string        `json:"name"`
+    Gender      string        `json:"gender"`
+    Company     string        `json:"company"`
+    Email       string        `json:"email"`
+    Phone       string        `json:"phone"`
+    Address     string        `json:"address"`
+    About       string        `json:"about"`
+    Registered  string        `json:"registered"`
+    Latitude    float32       `json:"Latitude"`
+    Longitude   float32       `json:"Longitude"`
+    Tags        []string      `json:"tags"`
+    Friends     []Friend      `json:"friends"`
+    Data        string        `json:"data"`
 }
